78d44ee64ffe814641ea40adf1e1a511177b1cf8
万睿皓 <wanruiha@maneiakaslaptop.myfiosgateway.com>
2024-01-06 16:18:57 -0500

    Added functionality

---
+++ cybulde/utils/data_utils.py
@@ -0,0 +1,21 @@
+from cybulde.utils.utils import run_shell_command, get_logger
+from pathlib import Path
+
+
+DATA_UTILS_LOGGER = get_logger(Path(__file__).name)
+
+
+def is_dvc_initialized() -> bool:
+    return (Path().cwd() / ".dvc").exists()
+
+
+def initialize_dvc() -> None:
+    if is_dvc_initialized():
+        DATA_UTILS_LOGGER.info("DVC is already initialized")
+        return
+    DATA_UTILS_LOGGER.info("Initializing DVC")
+    run_shell_command("dvc init")
+    run_shell_command("dvc config core.analytics false")
+    run_shell_command("dvc config core.autostage true")
+    run_shell_command("git add .dvc")
+    run_shell_command("git commit -m 'Initialized DVC'")
--- cybulde/version_data.py
+++ cybulde/version_data.py
@@ -1,11 +1,12 @@
 from cybulde.config_schemas.config_schema import Config
 from cybulde.utils.config_utils import get_config
+from cybulde.utils.data_utils import initialize_dvc
 
 
 @get_config(config_path="../configs", config_name="config")
-def entrypoint(config: Config) -> None:
-    print(config)
+def version_data(config: Config) -> None:
+    initialize_dvc()
 
 
 if __name__ == "__main__":
-    entrypoint()  # type: ignore
+    version_data()  # type: ignore
