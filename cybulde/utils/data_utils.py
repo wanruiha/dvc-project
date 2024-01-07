98f4de8a157ddf9799c74b00bbf470415154e76d
wanruiha <ruihaowan123@gmail.com>
2024-01-08 00:31:39 +0100

    Update version of the data from v0 to v1

--- cybulde/utils/data_utils.py
+++ cybulde/utils/data_utils.py
@@ -40,7 +40,7 @@ def commit_to_dvc(dvc_raw_data_folder: str, dvc_remote_name: str) -> None:
     run_shell_command(f"dvc add {dvc_raw_data_folder}")
     run_shell_command("git add .")
     run_shell_command(f"git commit -nm 'Update version of the data from v{current_version} to {next_version}'")
-    run_shell_command(f"git tag -a {next_version} -m 'Data version {next_version}")
+    run_shell_command(f"git tag -a {next_version} -m 'Data version {next_version}'")
     run_shell_command(f"dvc push {dvc_raw_data_folder}.dvc --remote {dvc_remote_name}")
     run_shell_command("git push --follow-tags")
     run_shell_command("git push -f --tags")
