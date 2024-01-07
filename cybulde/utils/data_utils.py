fea94c9a2b6c59ffe0a5d31a213d833d7c2de624
万睿皓 <wanruiha@maneiakaslaptop.myfiosgateway.com>
2024-01-06 20:55:38 -0500

    Fix a typo

--- cybulde/utils/data_utils.py
+++ cybulde/utils/data_utils.py
@@ -24,7 +24,7 @@ def initialize_dvc_storage(dvc_remote_name: str, dvc_remote_url: str) -> None:
     if not run_shell_command("dvc remote list"):
         DATA_UTILS_LOGGER.info("Initializing DVC Storage")
         run_shell_command(f"dvc remote add -d {dvc_remote_name} {dvc_remote_url}")
-        run_shell_command("git add ./dvc/config")
+        run_shell_command("git add .dvc/config")
         run_shell_command(f"git commit -nm 'Configured remote storage at: {dvc_remote_url}'")
     else:
         DATA_UTILS_LOGGER.info("DVC storage was already initialized...")
