b37bb95c9623eea5916f1d5031a10fadde87807a
万睿皓 <wanruiha@maneiakaslaptop.myfiosgateway.com>
2024-01-07 18:27:10 -0500

    Implement data version functions

--- cybulde/utils/data_utils.py
+++ cybulde/utils/data_utils.py
@@ -1,3 +1,5 @@
+from subprocess import CalledProcessError
+
 from cybulde.utils.utils import run_shell_command, get_logger
 from pathlib import Path
 
@@ -28,3 +30,28 @@ def initialize_dvc_storage(dvc_remote_name: str, dvc_remote_url: str) -> None:
         run_shell_command(f"git commit -nm 'Configured remote storage at: {dvc_remote_url}'")
     else:
         DATA_UTILS_LOGGER.info("DVC storage was already initialized...")
+
+
+def commit_to_dvc(dvc_raw_data_folder: str, dvc_remote_name: str) -> None:
+    current_version = ""
+    if not current_version:
+        current_version = "0"
+    next_version = f"v{int(current_version)+1}"
+    run_shell_command(f"dvc add {dvc_raw_data_folder}")
+    run_shell_command("git add .")
+    run_shell_command(f"git commit -nm 'Update version of the data from v{current_version} to {next_version}'")
+    run_shell_command(f"git tag -a {next_version} -m 'Data version {next_version}")
+    run_shell_command(f"dvc push {dvc_raw_data_folder}.dvc --remote {dvc_remote_name}")
+    run_shell_command("git push --follow-tags")
+    run_shell_command("git push -f --tags")
+
+
+def make_new_data_version(dvc_raw_data_folder: str, dvc_remote_name: str) -> None:
+    try:
+        status = run_shell_command(f"dvc status {dvc_raw_data_folder}.dvc")
+        if status == "Data and pipelines are up to data.\n":
+            DATA_UTILS_LOGGER.info("Data and pipelines are up to date")
+            return
+        commit_to_dvc(dvc_raw_data_folder, dvc_remote_name)
+    except CalledProcessError:
+        commit_to_dvc(dvc_raw_data_folder, dvc_remote_name)
--- cybulde/version_data.py
+++ cybulde/version_data.py
@@ -1,12 +1,13 @@
 from cybulde.config_schemas.config_schema import Config
 from cybulde.utils.config_utils import get_config
-from cybulde.utils.data_utils import initialize_dvc, initialize_dvc_storage
+from cybulde.utils.data_utils import initialize_dvc, initialize_dvc_storage, commit_to_dvc
 
 
 @get_config(config_path="../configs", config_name="config")
 def version_data(config: Config) -> None:
     initialize_dvc()
     initialize_dvc_storage(config.dvc_remote_name, config.dvc_remote_url)
+    commit_to_dvc(config.dvc_raw_data_folder, config.dvc_remote_name)
 
 
 if __name__ == "__main__":
