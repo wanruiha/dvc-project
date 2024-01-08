af216eb72b4f06f8a5ce91c48d2c52bf7feb476b
wanruiha <ruihaowan123@gmail.com>
2024-01-08 06:10:17 +0100

    Update version of the data from v1 to v2

--- cybulde/utils/data_utils.py
+++ cybulde/utils/data_utils.py
@@ -33,7 +33,7 @@ def initialize_dvc_storage(dvc_remote_name: str, dvc_remote_url: str) -> None:
 
 
 def commit_to_dvc(dvc_raw_data_folder: str, dvc_remote_name: str) -> None:
-    current_version = ""
+    current_version = run_shell_command("git tag --list | sort -t v -k 2 -g | tail -1 | sed 's/v//'").strip()
     if not current_version:
         current_version = "0"
     next_version = f"v{int(current_version)+1}"
